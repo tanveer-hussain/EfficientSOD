5d3db60597f089226a247111c6e8499bff04a84e
Tanveer Hussain <40714349+tanveer-hussain@users.noreply.github.com>
2024-01-08 09:37:20 +0000

    Updates

--- JournalExtension/SODModel.py
+++ JournalExtension/SODModel.py
@@ -6,57 +6,83 @@ from torch_geometric.data import Data
 from sklearn.neighbors import NearestNeighbors
 
 from ResNet import B2_ResNet
+import torchvision.models as models
+
 class GATSegmentationModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_heads):
+    def __init__(self, training):
         super(GATSegmentationModel, self).__init__()
 
         self.resnet = B2_ResNet()
 
-        self.conv1 = GATConv(input_dim, hidden_dim, heads=num_heads)
-        self.conv2 = GATConv(hidden_dim * num_heads, 1, heads=1)  # Output layer with 1 channel
+        self.training = training
+        if self.training:
+            self.initialize_weights()
 
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
+        # self.conv1 = GATConv(input_dim, hidden_dim, heads=num_heads)
+        # self.conv2 = GATConv(hidden_dim * num_heads, 1, heads=1)  # Output layer with 1 channel
 
-        x = F.relu(self.conv1(x, edge_index))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = self.conv2(x, edge_index)
+    def image_to_graph(self, input):
+        # Reshape image into a vector
+        x = input.view(-1, 1).float().numpy()
 
-        return x.view(-1, 256, 256)  # Reshape output to a 256x256 image
+        # Compute nearest neighbors
+        nn_model = NearestNeighbors(n_neighbors=4)  # Define the number of neighbors
+        nn_model.fit(x)
+        distances, indices = nn_model.kneighbors(x)
 
+        # Creating edge_index from nearest neighbors indices
+        edge_index = []
+        for i in range(len(indices)):
+            for j in range(1, len(indices[i])):
+                edge_index.append((i, indices[i][j]))
 
-def image_to_graph(image):
-    # Reshape image into a vector
-    x = image.view(-1, 1).float().numpy()
+        edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
 
-    # Compute nearest neighbors
-    nn_model = NearestNeighbors(n_neighbors=4)  # Define the number of neighbors
-    nn_model.fit(x)
-    distances, indices = nn_model.kneighbors(x)
+        # Converting image to graph representation
+        x = input.view(-1, 1)
+        data = Data(x=x, edge_index=edge_index)
 
-    # Creating edge_index from nearest neighbors indices
-    edge_index = []
-    for i in range(len(indices)):
-        for j in range(1, len(indices[i])):
-            edge_index.append((i, indices[i][j]))
+        return data
 
-    edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
+    def forward(self, input):
+        x = self.resnet.conv1(input)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
+        x = self.resnet.maxpool(x)
 
-    # Converting image to graph representation
-    x = image.view(-1, 1)
-    data = Data(x=x, edge_index=edge_index)
+        # x, edge_index = data.x, data.edge_index
+        #
+        # x = F.relu(self.conv1(x, edge_index))
+        # x = F.dropout(x, p=0.5, training=self.training)
+        # x = self.conv2(x, edge_index)
+        # x.view(-1, 256, 256)  # Reshape output to a 256x256 image
 
-    return data
+        return x
 
-# Generating a random image tensor (256x256)
-random_image = torch.rand((256, 256))
-data = image_to_graph(random_image)
+    def initialize_weights(self):
+        print('Loading weights...')
+        res50 = models.resnet101(pretrained=True)
+        pretrained_dict = res50.state_dict()
+        all_params = {}
+        for k, v in self.resnet.state_dict().items():
+            if k in pretrained_dict.keys():
+                v = pretrained_dict[k]
+                all_params[k] = v
+            elif '_1' in k:
+                name = k.split('_1')[0] + k.split('_1')[1]
+                v = pretrained_dict[name]
+                all_params[k] = v
+            elif '_2' in k:
+                name = k.split('_2')[0] + k.split('_2')[1]
+                v = pretrained_dict[name]
+                all_params[k] = v
+        assert len(all_params.keys()) == len(self.resnet.state_dict().keys())
+        print(self.resnet.load_state_dict(all_params))
 
-# Model initialization and inference
-input_dim = 1  # Single-channel image
-hidden_dim = 64
-num_heads = 4
+model = GATSegmentationModel(training=True)
+tensor = torch.randn((2,3,256,256))
+with torch.no_grad():
+    out = model(tensor)
+    print (out.shape)
 
-model = GATSegmentationModel(input_dim, hidden_dim, num_heads)
-output = model(data)
-print(output.shape)  # Output shape (batch_size, height, width)
+print ('Done')
